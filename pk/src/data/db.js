7c06fede08ca5a6d953992725be7be32b1160315
PK <pedrojugador2@hotmail.com>
2024-05-01 12:49:28 -0400

    Initial Commit

---
+++ pk/src/data/db.js
@@ -0,0 +1,63 @@
+export const db = [
+    {
+      id: 1,
+      nombre: 'Almacen Encarnación Suc.1',
+      direccion: 'Zona Uni, Encarnación',
+      contacto: '0985 753536',
+    },
+    {
+      id: 2,
+      nombre: 'Almacen Encarnación Casa Matriz',
+      direccion: 'Barrio Obrero, Encarnación',
+      contacto: '0971 234567',
+    },
+    {
+      id: 3,
+      nombre: 'Depósito Ciudad del Este Suc.2',
+      direccion: 'Centro Histórico, Ciudad del Este',
+      contacto: '0982 345678',
+    },
+    {
+      id: 4,
+      nombre: 'Depósito Pedro Juan Caballero Suc.3',
+      direccion: 'Manorá, Pedro Juan Caballero',
+      contacto: '0961 456789',
+    },
+    {
+      id: 5,
+      nombre: 'Depósito Coronel Oviedo Suc.4',
+      direccion: 'Las Mercedes, Coronel Oviedo',
+      contacto: '0952 567890',
+    },
+    {
+      id: 6,
+      nombre: 'Depósito Villarrica Suc.5',
+      direccion: 'San Pablo, Villarrica',
+      contacto: '0943 678901',
+    },
+    {
+      id: 7,
+      nombre: 'Depósito San Lorenzo Suc.6',
+      direccion: 'Villa Morra, San Lorenzo',
+      contacto: '0974 789012',
+    },
+    {
+      id: 8,
+      nombre: 'Depósito Fernando de la Mora Suc.7',
+      direccion: 'Carmelitas, Fernando de la Mora',
+      contacto: '0992 890123',
+    },
+    {
+      id: 9,
+      nombre: 'Depósito Asunción Suc.8',
+      direccion: 'San Lorenzo, Asunción',
+      contacto: '0983 901234',
+    },
+    {
+      id: 10,
+      nombre: 'Depósito Luque Suc.9',
+      direccion: 'Luque',
+      contacto: '0991 012345',
+    }
+  ];
+  
